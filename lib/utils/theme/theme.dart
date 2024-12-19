dc86de00c85dff4d49c4565b114e7e788f4ae8a8
labibul.ahsan.alif <labibul.ahsan.alif@g.bracu.ac.bd>
2024-12-19 20:35:24 +0000

    Theme added

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MAppTheme {
+  MAppTheme._();
+}
+
+static ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  fontFamily: 'Poppins',
+  brightness: Brightness.light,
+  primaryColor: Colors.blue,
+  scaffoldBackgroundColor: ,
+  textTheme: 
+
+);
+
+static ThemeData darkTheme = ThemeData();
